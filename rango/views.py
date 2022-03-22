b96627086d71d483b621e54ee7e62fd9f850ed20
cammycon <93663674+cammycon@users.noreply.github.com>
2022-03-22 12:00:53 +0000

    chapter 4 and exercise complete

--- rango/views.py
+++ rango/views.py
@@ -6,4 +6,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return render(request, 'rango/about.html')
+    context_dict = {'boldmessage': 'This tutorial has been put together by Cameron Constable'}
+    return render(request, 'rango/about.html', context=context_dict)
